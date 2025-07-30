bb3db89124a70ff28b2064cf5eedbc36283a2ebc
Shiyi Tan <159016858+shiyitan11@users.noreply.github.com>
2025-07-30 20:53:37 +0100

    EnglishReadability.py

--- EnglishReadability.py
+++ EnglishReadability.py
@@ -9,7 +9,7 @@ flesch_reading = textstat.flesch_reading_ease(test_data)
 fk_grade = textstat.flesch_kincaid_grade(test_data)
 gunning_fog = textstat.gunning_fog(test_data)
 
-print ("SMOG Index:", smog) 
+print("SMOG Index:", smog) 
 print("Flesch Reading Ease:", flesch_reading)  
 print("Flesch-Kincaid Grade Level:", fk_grade) 
 print("Gunning Fog Index:", gunning_fog) 
